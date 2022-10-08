fd17ce3f4d8ff43e265025e65a99498d168f7068
wangl <123456>
2022-10-08 10:53:06 +0800

    项目初始化

---
+++ src/main/java/com/liang/wang/Main.java
@@ -0,0 +1,25 @@
+package com.liang.wang;
+
+/**
+ * 　　　　　　 ＿＿＿
+ * 　　　　　／＞　　フ ＼
+ * 　　　　　|  　_　 _ l
+ * 　 　　　／  ミ＿xノ
+ * 　　 　 /　　　 　 |
+ * 　　　 /　 ヽ　　 ﾉ
+ * 　 　 │　　|　|　|
+ * 　／￣|　　 |　|　|
+ * 　| (￣ヽ＿_ヽ_)__)
+ * 　＼二つ
+ *
+ * @author wangl
+ * @title: ${NAME}
+ * @projectName ${PROJECT_NAME}
+ * @description: TODO
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
